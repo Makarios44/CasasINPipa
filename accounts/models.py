6b0de2cd2538bcaa8e0cf87e927bdc277e684978
makarios44 <mmakarios44@gmail.com>
2025-01-09 12:03:11 -0300

    autenticação feita

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User:
+
     nome = models.CharField(max_length=50, null=False)
     sobrenome = models.CharField(max_length=50, null=False) 
     email = models.EmailField(max_length=254, null=False, unique=True)
@@ -9,5 +10,4 @@ class User:
     password2 = models.CharField( max_length=50)
     telefone = models.CharField(max_length=13, null=False)
     data_nascimento = models.DateField(null=False)
-  
     
--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,62 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib import messages
 
 # Create your views here.
 def login(request):
-    if request.method == 'GET':
-        return render(request,'login.html')
+    if request.method == 'POST':
+   
+        nome = request.POST.get('login')
+        password = request.POST.get('senha')
+
+        usuario  = authenticate(username=nome, password=password)
+        
+
+        if usuario:
+             
+            messages.success(request,'seja bem vindo')
+            return  render(request,'reservas.html')
+     
+        else:
+            messages.error(request,'email ou senha incorretos')
     
-    elif request.method == 'POST':
+    return render(request,'login.html')
+
        
-        return  render(request,'reservas.html')  
-    
+
 
 def register(request):
     if request.method == 'GET':
-        return render(request,'register.html')
+        return render(request, 'register.html')
     
     elif request.method == 'POST':
-         return render(request,'login.html')
+        # Captura os dados do formulário
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        email = request.POST.get('email')
+        password = request.POST.get('senha')
+        password2 = request.POST.get('confirme_senha')
+        telefone = request.POST.get('telefone')
+        data_nascimento = request.POST.get('data_nascimento')
+
+       
+        if User.objects.filter(username=nome).exists():
+            messages.error(request, 'Este usuario já está cadastrado')
+        elif password != password2:
+            messages.error(request, 'As senhas não são iguais')
+        else:
+            
+            User.objects.create_user(
+                username=nome, 
+                email=email,
+                password=password,
+                last_name=sobrenome,
+            )
+            messages.success(request, 'Usuário cadastrado com sucesso')
+            return render(request, 'login.html') 
+
+    return render(request, 'register.html')
 
 
 
